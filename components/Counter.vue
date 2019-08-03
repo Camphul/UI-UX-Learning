3d01ec32f36e6ca4a1bf84734cba6bdda30f50f7
Luca Camphuisen <luca.camphuisen@hva.nl>
2019-08-04 01:19:49 +0200

    Simple components created.

---
+++ components/Counter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Start index: {{start}}
+  </div>
+</template>
+<script lang="ts">
+  import {Vue, Component, Prop} from "nuxt-property-decorator";
+
+  @Component
+  export default class Counter extends Vue{
+    @Prop({required: false, default: 0})
+    start!: number
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,27 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
-        dumb-learner
+        Counter App
       </h1>
       <h2 class="subtitle">
-        Learning nuxtjs with typescript and vuetify
+        <Counter/>
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
+<script lang="ts">
+  import {Component, Vue} from "nuxt-property-decorator";
+  import Counter from '@/components/Counter.vue'
+  //import Counter from '@/components/'
+  @Component({
+    components: {
+      Counter
+    }
+  })
+  export default class IndexPage extends Vue {
   }
-}
 </script>
 
 <style>
@@ -64,9 +50,5 @@ export default {
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
