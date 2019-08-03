1e8fbd96331da63348f9c44f4256c6bb4f02c345
Luca Camphuisen <luca.camphuisen@hva.nl>
2019-08-04 01:48:51 +0200

    Simple counter app.

--- components/Counter.vue
+++ components/Counter.vue
@@ -1,14 +1,42 @@
 <template>
   <div>
-    Start index: {{start}}
+    Start index: {{count}}<br>
+    <toggle-button @onEnabled="startCounter" @onDisabled="stopCounter"/>
   </div>
 </template>
 <script lang="ts">
-  import {Vue, Component, Prop} from "nuxt-property-decorator";
+  import {Vue, Component, Prop, Emit} from "nuxt-property-decorator";
+  import ToggleButton from "@/components/ToggleButton.vue";
 
-  @Component
+  @Component({
+    components: {
+      ToggleButton
+    }
+  })
   export default class Counter extends Vue{
-    @Prop({required: false, default: 0})
-    start!: number
+    @Prop({required: false, default: 5})
+    readonly start!: number;
+
+    count: number = 0;
+    intervalId: any;
+
+    mounted() {
+      this.count = this.start;
+    }
+
+    @Emit('onIncrement')
+    increment() {
+      this.count++;
+    }
+
+    @Emit('onStart')
+    startCounter() {
+      this.intervalId = setInterval(this.increment, 1000);
+    }
+
+    @Emit('onStop')
+    stopCounter() {
+      clearInterval(this.intervalId)
+    }
   }
 </script>
---
+++ components/ToggleButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button @click="toggle">{{text}}</button>
+</template>
+<script lang="ts">
+  import {Component, Emit, Prop, Vue, Watch} from "nuxt-property-decorator";
+
+  @Component
+  export default class ToggleButton extends Vue {
+    @Prop({required: false, default: 'Stop'})
+    readonly enabledText!: string;
+    @Prop({required: false, default: 'Start'})
+    readonly disabledText!: string;
+
+    enabled: boolean = false;
+
+    get text() {
+      return this.enabled ? this.enabledText : this.disabledText;
+    }
+
+    @Watch('enabled')
+    handlerStateChange(value: boolean) {
+      if(value) {
+        this.onEnabled()
+      } else {
+        this.onDisabled()
+      }
+    }
+
+    @Emit('onEnabled')
+    onEnabled() {
+    }
+
+    @Emit('onDisabled')
+    onDisabled() {
+    }
+
+    toggle() {
+      this.enabled = !this.enabled;
+    }
+  }
+</script>
