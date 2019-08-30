09477a9e073f7fc4d27c6bd5d33c78f432e8a013
Luca Camphuisen <luca.camphuisen@hva.nl>
2019-08-30 19:30:19 +0200

    Easier navigation drawer items.

--- components/bar/TNavigationDrawer.vue
+++ components/bar/TNavigationDrawer.vue
@@ -2,23 +2,25 @@
   <v-navigation-drawer v-model="open" app clipped left>
     <v-list dense>
       <slot name="items">
-        <v-list-item to="/create">
-          <v-list-item-action>
-            <v-icon>mdi-pencil-plus</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Create BlogPost</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+        <t-navigation-drawer-item
+          to="/create"
+          icon="mdi-pencil-plus"
+          title="Create BlogPost"
+        />
       </slot>
     </v-list>
   </v-navigation-drawer>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
+import TNavigationDrawerItem from './TNavigationDrawerItem.vue'
 import { Toggleable } from '~/lib/types'
 
-@Component
+@Component({
+  components: {
+    TNavigationDrawerItem
+  }
+})
 export default class TNavigationDrawer extends Vue implements Toggleable {
   private open: boolean = false
 
---
+++ components/bar/TNavigationDrawerItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-list-item :to="to">
+    <v-list-item-action v-show="icon">
+      <v-icon>
+        {{ icon }}
+      </v-icon>
+    </v-list-item-action>
+    <v-list-item-content>
+      <v-list-item-title>
+        {{ title }}
+      </v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class TNavigationDrawerItem extends Vue {
+  @Prop({
+    required: true
+  })
+  readonly to: string = '/'
+
+  @Prop({
+    required: true
+  })
+  readonly title: string = 'Untitled'
+
+  @Prop({
+    required: false
+  })
+  readonly icon: string = ''
+}
+</script>
